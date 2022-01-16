59af7ebfc684b5bb157b298e3e2b025899e3b40a
lhjwork <jin.come.up.business@gmail.com>
2022-01-17 01:04:44 +0900

    Rooms model working

--- rooms/models.py
+++ rooms/models.py
@@ -1,10 +1,14 @@
 from importlib.metadata import packages_distributions
+import os 
 from django.db import models
 from core import models as core_models
-
+from django_countries.fields import CountryField
 # Create your models here.
 
 class Room(core_models.TimeStampedModel):
-  pass
   
+  name = models.CharField(max_length=140)
+  description = models.TextField()
+  country = CountryField()
+  city = models.CharField(max_length=100)
   
