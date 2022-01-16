fd5d7c811c59ef4c5798452d8660991feb77da6e
lhjwork <jin.come.up.business@gmail.com>
2022-01-16 23:59:46 +0900

    TimeStampedModel and add core

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,10 @@
+from importlib.metadata import packages_distributions
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+class Room(core_models.TimeStampedModel):
+  pass
+  
+  
